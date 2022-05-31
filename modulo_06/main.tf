0b3abe0846679e82b3661d4a53ab1992d816da03
MatheusXavier <matheusxxavier@gmail.com>
2022-05-31 20:33:51 -0300

    Removing key from file

--- modulo_06/main.tf
+++ modulo_06/main.tf
@@ -3,7 +3,7 @@ terraform {
     resource_group_name  = "BeWelcome-Development"
     storage_account_name = "bewelcomestoragedev"
     container_name       = "terraformstate"
-    key                  = "pGItxp0iS7RFz/Eq1jN44yq7eCo7Fs4zxaC8NKnhSijM10DWSf3yvKJZ2JBfZ671FPhlPQvSjOHUCzv6p9Z39g=="
+    key                  = "[yourkeygoeshere]"
   }
 }
 
